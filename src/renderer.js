817fa682fabf730a90411b005c8480360d586503
n-devs <n.devs260340@gmail.com>
2023-06-21 07:42:06 +0700

    update

--- src/renderer.js
+++ src/renderer.js
@@ -6,14 +6,16 @@ const onStopEl = document.getElementById("onStop");
 const fileEl = document.getElementById("file");
 
 onStartEl.onclick = () => {
-  statusEl.innerHTML = "online";
+  statusEl.innerHTML = "online".toUpperCase();
+  statusEl.style.color = 'green';
   onStartEl.style.display = "none";
   onStopEl.style.display = "block";
   ipcRenderer.send("update-status", true,fileEl.files[0].path);
 };
 
 onStopEl.onclick = () => {
-  statusEl.innerHTML = "stoped";
+  statusEl.innerHTML = "stoped".toUpperCase();
+  statusEl.style.color = 'red';
   onStartEl.style.display = "block";
   onStopEl.style.display = "none";
   ipcRenderer.send("update-status", false,fileEl.files[0].path);
@@ -21,16 +23,19 @@ onStopEl.onclick = () => {
 
 ipcRenderer.on("on-status", (event, list) => {
   if (list.length === 0) {
-    statusEl.innerHTML = "stoped";
+    statusEl.innerHTML = "stoped".toUpperCase();
+    statusEl.style.color = 'red';
     onStartEl.style.display = "block";
     onStopEl.style.display = "none";
   } else {
     if (list[0].pm2_env.status === "online") {
-      statusEl.innerHTML = list[0].pm2_env.status;
+      statusEl.innerHTML = list[0].pm2_env.status.toUpperCase();
+      statusEl.style.color = 'green';
       onStartEl.style.display = "none";
       onStopEl.style.display = "block";
     } else {
-      statusEl.innerHTML = list[0].pm2_env.status;
+      statusEl.innerHTML = list[0].pm2_env.status.toUpperCase();
+      statusEl.style.color = 'red';
       onStartEl.style.display = "block";
       onStopEl.style.display = "none";
     }
